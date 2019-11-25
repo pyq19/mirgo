3e731bd54d7803eab8e1166ec9671c6e96b2c44e
pyq <pyq225@gmail.com>
2019-11-25 17:06:21 +0800

    init dotnettools

---
+++ README.md
@@ -0,0 +1,3 @@
+# 使用 go 语言实现 https://github.com/Suprcode/mir2
+
+1. 用 dotnet-tools 将原项目的文件数据库里的数据转到 MySQL
---
+++ dotnettools/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnettools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
