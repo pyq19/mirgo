77cb65e00e7e627380daefd1a25a375a24363548
pyq <pyq225@gmail.com>
2019-11-26 15:49:16 +0800

    update
    Former-commit-id: fdab9b7b8a9b574b50a4e88245f1522cbe85a179

--- dotnettools/Program.cs
+++ dotnettools/Program.cs
@@ -8,7 +8,7 @@ namespace dotnettools
         {
             Loader loader = new Loader();
             string path = "/opt/gopath/src/github.com/yenkeia/mir-go/dotnettools/database/Server.MirDB";
-            loader.loadDB(path);
+            loader.loadFromFile(path);
             Console.WriteLine("加载完成");
         }
     }
