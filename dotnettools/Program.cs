d7effd96149f28bb8a2838f1acdc7696a557d90c
pyq <pyq225@gmail.com>
2019-11-30 14:47:56 +0800

    update

--- dotnettools/Program.cs
+++ dotnettools/Program.cs
@@ -7,11 +7,15 @@ namespace dotnettools
     {
         static void Main(string[] args)
         {
-            string connectionString = "server=127.0.0.1;uid=root;pwd=root;database=mir";
+            // string connectionString = "server=127.0.0.1;uid=root;pwd=root;database=mir";
+            string projectDirPath = System.Environment.CurrentDirectory;
+            string sqlitePath = "/src/github.com/yenkeia/mir-go/dotnettools/mir.sqlite";
+            string connectionString = "Data Source=" + projectDirPath + sqlitePath;
             SqlSugarClient DB = new SqlSugarClient(new ConnectionConfig()
             {
                 ConnectionString = connectionString,
-                DbType = DbType.MySql,//设置数据库类型
+                // DbType = DbType.MySql,//设置数据库类型
+                DbType = DbType.Sqlite,//设置数据库类型
                 IsAutoCloseConnection = true,//自动释放数据务，如果存在事务，在事务结束后释放
                 InitKeyType = InitKeyType.Attribute //从实体特性中读取主键自增列信息
             });
@@ -19,7 +23,7 @@ namespace dotnettools
 
             Manager manager = new Manager();
 
-            string path = "/opt/gopath/src/github.com/yenkeia/mir-go/dotnettools/database/Server.MirDB";
+            string path = projectDirPath + "/src/github.com/yenkeia/mir-go/dotnettools/database/Server.MirDB";
             manager.loadFromFile(path);
 
             manager.saveDataToDatabase();
