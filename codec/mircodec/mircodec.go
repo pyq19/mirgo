499bf475aae8d40535de5dbc636bf1d9db684d63
pyq <pyq225@gmail.com>
2019-12-04 12:09:08 +0800

    update
    Former-commit-id: 74993073551db32d96ad15785fa6bfc067925dec

--- codec/mircodec/mircodec.go
+++ codec/mircodec/mircodec.go
@@ -3,40 +3,32 @@ package mircodec
 import (
 	"github.com/davyxu/cellnet"
 	"github.com/davyxu/cellnet/codec"
-	"github.com/davyxu/goobjfmt"
 )
 
+// MirCodec 编码解码
 type MirCodec struct {
 }
 
+// Name 返回名字
 func (m *MirCodec) Name() string {
 	return "MirCodec"
 }
 
+// MimeType 我也不知道是干嘛的
 func (m *MirCodec) MimeType() string {
 	return "application/binary"
 }
 
-//func (m *MirCodec) Encode(msgObj interface{}, ctx cellnet.ContextSet) (data interface{}, err error) {
-//	// TODO 将数据转换为字节数组
-//	return nil, nil
-//}
-//
-//func (m *MirCodec) Decode(data interface{}, msgObj interface{}) error {
-//	// TODO 将字节数组转换为数据
-//	return nil
-//}
+// Encode 将数据转换为字节数组
+func (m *MirCodec) Encode(msgObj interface{}, ctx cellnet.ContextSet) (data interface{}, err error) {
+	return nil, nil
+}
+
+// Decode 将字节数组转换为数据
+func (m *MirCodec) Decode(data interface{}, msgObj interface{}) error {
+	return nil
+}
 
 func init() {
 	codec.RegisterCodec(new(MirCodec))
-}
-
-
-func (self *MirCodec) Encode(msgObj interface{}, ctx cellnet.ContextSet) (data interface{}, err error) {
-	return goobjfmt.BinaryWrite(msgObj)
-}
-
-func (self *MirCodec) Decode(data interface{}, msgObj interface{}) error {
-
-	return goobjfmt.BinaryRead(data.([]byte), msgObj)
 }
