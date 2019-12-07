c59ce3d7b936c50c84ede2e08fc7ee72f6dfd0e8
pyq <pyq225@gmail.com>
2019-12-07 13:56:55 +0800

    update

--- server/smain.go
+++ server/smain.go
@@ -1,32 +1,6 @@
 package main
 
-import (
-	"github.com/davyxu/cellnet"
-	"github.com/davyxu/cellnet/peer"
-	_ "github.com/davyxu/cellnet/peer/tcp"
-	"github.com/davyxu/cellnet/proc"
-	"github.com/davyxu/golog"
-	_ "github.com/yenkeia/mirgo/proc/mirtcp"
-)
-
-var log = golog.New("server")
-
 func main() {
-
-	queue := cellnet.NewEventQueue()
-
-	addr := "0.0.0.0:7000"
-	p := peer.NewGenericPeer("tcp.Acceptor", "server", addr, queue)
-
-	proc.BindProcessorHandler(p, "mir.server.tcp", eventHandler)
-
-	// 开始侦听
-	p.Start()
-
-	// 事件队列开始循环
-	queue.StartLoop()
-
-	// 阻塞等待事件队列结束退出( 在另外的goroutine调用queue.StopLoop() )
-	queue.Wait()
-
+	g := NewGame()
+	g.ServerStart()
 }
