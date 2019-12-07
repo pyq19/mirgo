d4576759a89a3ce7c43ea89f091bd3df2ff53a2e
pyq <pyq225@gmail.com>
2019-12-07 13:02:00 +0800

    update
    Former-commit-id: 25c15ce24cb87573670701972321605751fce28f

--- proto/client/cmsg.go
+++ proto/client/cmsg.go
@@ -14,14 +14,59 @@ const (
 	CLIENT_VERSION = 1000 + iota
 	DISCONNECT
 	KEEP_ALIVE
+	NEW_ACCOUNT
+	CHANGE_PASSWORD
+	LOGIN
+	NEW_CHARACTER
+	DELETE_CHARACTER
+	START_GAME
 )
 
 type ClientVersion struct {
 	VersionHash []uint8
 }
 
+type Disconnect struct{}
+
 type KeepAlive struct {
 	Time int64
+}
+
+// TODO
+type NewAccount struct {
+	//public string AccountID = string.Empty
+	//public string Password = string.Empty
+	//public DateTime BirthDate
+	//public string UserName = string.Empty
+	//public string SecretQuestion = string.Empty
+	//public string SecretAnswer = string.Empty
+	//public string EMailAddress = string.Empty
+}
+
+type ChangePassword struct {
+	AccountID       string
+	CurrentPassword string
+	NewPassword     string
+}
+
+type Login struct {
+	AccountID string
+	Password  string
+}
+
+// TODO
+type NewCharacter struct {
+	//public string Name = string.Empty;
+	//public MirGender Gender;
+	//public MirClass Class;
+}
+
+type DeleteCharacter struct {
+	CharacterIndex int16
+}
+
+type StartGame struct {
+	CharacterIndex int16
 }
 
 // 引用消息时，自动注册消息，这个文件可以由代码生成自动生成
@@ -34,10 +79,44 @@ func init() {
 		Type:  reflect.TypeOf((*ClientVersion)(nil)).Elem(),
 		ID:    CLIENT_VERSION,
 	})
-
+	cellnet.RegisterMessageMeta(&cellnet.MessageMeta{
+		Codec: mirCodec,
+		Type:  reflect.TypeOf((*Disconnect)(nil)).Elem(),
+		ID:    DISCONNECT,
+	})
 	cellnet.RegisterMessageMeta(&cellnet.MessageMeta{
 		Codec: mirCodec,
 		Type:  reflect.TypeOf((*KeepAlive)(nil)).Elem(),
 		ID:    KEEP_ALIVE,
+	})
+	cellnet.RegisterMessageMeta(&cellnet.MessageMeta{
+		Codec: mirCodec,
+		Type:  reflect.TypeOf((*NewAccount)(nil)).Elem(),
+		ID:    NEW_ACCOUNT,
+	})
+	cellnet.RegisterMessageMeta(&cellnet.MessageMeta{
+		Codec: mirCodec,
+		Type:  reflect.TypeOf((*ChangePassword)(nil)).Elem(),
+		ID:    CHANGE_PASSWORD,
+	})
+	cellnet.RegisterMessageMeta(&cellnet.MessageMeta{
+		Codec: mirCodec,
+		Type:  reflect.TypeOf((*Login)(nil)).Elem(),
+		ID:    LOGIN,
+	})
+	cellnet.RegisterMessageMeta(&cellnet.MessageMeta{
+		Codec: mirCodec,
+		Type:  reflect.TypeOf((*NewCharacter)(nil)).Elem(),
+		ID:    NEW_CHARACTER,
+	})
+	cellnet.RegisterMessageMeta(&cellnet.MessageMeta{
+		Codec: mirCodec,
+		Type:  reflect.TypeOf((*DeleteCharacter)(nil)).Elem(),
+		ID:    DELETE_CHARACTER,
+	})
+	cellnet.RegisterMessageMeta(&cellnet.MessageMeta{
+		Codec: mirCodec,
+		Type:  reflect.TypeOf((*StartGame)(nil)).Elem(),
+		ID:    START_GAME,
 	})
 }
