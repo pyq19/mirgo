25c15ce24cb87573670701972321605751fce28f
pyq <pyq225@gmail.com>
2019-12-07 13:02:00 +0800

    update

--- proto/server/smsg.go
+++ proto/server/smsg.go
@@ -15,6 +15,17 @@ const (
 	CLIENT_VERSION
 	DISCONNECT
 	KEEP_ALIVE
+	NEW_ACCOUNT
+	CHANGE_PASSWORD
+	CHANGE_PASSWORD_BANNED
+	LOGIN
+	LOGIN_BANNED
+	LOGIN_SUCCESS
+	NEW_CHARACTER
+	NEW_CHARACTER_SUCCESS
+	DELETE_CHARACTER
+	DELETE_CHARACTER_SUCCESS
+	START_GAME
 )
 
 type Connected struct{}
@@ -23,8 +34,111 @@ type ClientVersion struct {
 	Result uint8
 }
 
+type Disconnect struct {
+	Reason uint8
+	/*
+	 * 0: Server Closing.
+	 * 1: Another User.
+	 * 2: Packet Error.
+	 * 3: Server Crashed.
+	 */
+}
+
 type KeepAlive struct {
 	Time int64
+}
+
+type NewAccount struct {
+	Result uint8
+	/*
+	 * 0: Disabled
+	 * 1: Bad AccountID
+	 * 2: Bad Password
+	 * 3: Bad Email
+	 * 4: Bad Name
+	 * 5: Bad Question
+	 * 6: Bad Answer
+	 * 7: Account Exists.
+	 * 8: Success
+	 */
+}
+
+type ChangePassword struct {
+	Result uint8
+	/*
+	 * 0: Disabled
+	 * 1: Bad AccountID
+	 * 2: Bad Current Password
+	 * 3: Bad New Password
+	 * 4: Account Not Exist
+	 * 5: Wrong Password
+	 * 6: Success
+	 */
+}
+
+// TODO
+type ChangePasswordBanned struct {
+	//public string Reason = string.Empty;
+	//public DateTime ExpiryDate;
+}
+
+type Login struct {
+	Result uint8
+	/*
+	* 0: Disabled
+	* 1: Bad AccountID
+	* 2: Bad Password
+	* 3: Account Not Exist
+	* 4: Wrong Password
+	 */
+}
+
+// TODO
+type LoginBanned struct {
+	//public string Reason = string.Empty;
+	//public DateTime ExpiryDate
+}
+
+// TODO
+type LoginSuccess struct{}
+
+type NewCharacter struct {
+	Result uint8
+	/*
+	 * 0: Disabled.
+	 * 1: Bad Character Name
+	 * 2: Bad Gender
+	 * 3: Bad Class
+	 * 4: Max Characters
+	 * 5: Character Exists.
+	 * */
+}
+
+// TODO
+type NewCharacterSuccess struct{}
+
+type DeleteCharacter struct {
+	Result uint8
+	/*
+	 * 0: Disabled.
+	 * 1: Character Not Found
+	 * */
+}
+
+type DeleteCharacterSuccess struct {
+	CharacterIndex int16
+}
+
+type StartGame struct {
+	Result uint8
+	/*
+	 * 0: Disabled.
+	 * 1: Not logged in
+	 * 2: Character not found.
+	 * 3: Start Game Error
+	 * */
+
+	Resolution int16
 }
 
 // 引用消息时，自动注册消息，这个文件可以由代码生成自动生成
@@ -37,16 +151,74 @@ func init() {
 		Type:  reflect.TypeOf((*Connected)(nil)).Elem(),
 		ID:    CONNECTED,
 	})
-
 	cellnet.RegisterMessageMeta(&cellnet.MessageMeta{
 		Codec: mirCodec,
 		Type:  reflect.TypeOf((*ClientVersion)(nil)).Elem(),
 		ID:    CLIENT_VERSION,
 	})
-
+	cellnet.RegisterMessageMeta(&cellnet.MessageMeta{
+		Codec: mirCodec,
+		Type:  reflect.TypeOf((*Disconnect)(nil)).Elem(),
+		ID:    DISCONNECT,
+	})
 	cellnet.RegisterMessageMeta(&cellnet.MessageMeta{
 		Codec: mirCodec,
 		Type:  reflect.TypeOf((*KeepAlive)(nil)).Elem(),
 		ID:    KEEP_ALIVE,
+	})
+	cellnet.RegisterMessageMeta(&cellnet.MessageMeta{
+		Codec: mirCodec,
+		Type:  reflect.TypeOf((*NewAccount)(nil)).Elem(),
+		ID:    NEW_ACCOUNT,
+	})
+	cellnet.RegisterMessageMeta(&cellnet.MessageMeta{
+		Codec: mirCodec,
+		Type:  reflect.TypeOf((*ChangePassword)(nil)).Elem(),
+		ID:    CHANGE_PASSWORD,
+	})
+	cellnet.RegisterMessageMeta(&cellnet.MessageMeta{
+		Codec: mirCodec,
+		Type:  reflect.TypeOf((*ChangePasswordBanned)(nil)).Elem(),
+		ID:    CHANGE_PASSWORD_BANNED,
+	})
+	cellnet.RegisterMessageMeta(&cellnet.MessageMeta{
+		Codec: mirCodec,
+		Type:  reflect.TypeOf((*Login)(nil)).Elem(),
+		ID:    LOGIN,
+	})
+	cellnet.RegisterMessageMeta(&cellnet.MessageMeta{
+		Codec: mirCodec,
+		Type:  reflect.TypeOf((*LoginBanned)(nil)).Elem(),
+		ID:    LOGIN_BANNED,
+	})
+	cellnet.RegisterMessageMeta(&cellnet.MessageMeta{
+		Codec: mirCodec,
+		Type:  reflect.TypeOf((*LoginSuccess)(nil)).Elem(),
+		ID:    LOGIN_SUCCESS,
+	})
+	cellnet.RegisterMessageMeta(&cellnet.MessageMeta{
+		Codec: mirCodec,
+		Type:  reflect.TypeOf((*NewCharacter)(nil)).Elem(),
+		ID:    NEW_CHARACTER,
+	})
+	cellnet.RegisterMessageMeta(&cellnet.MessageMeta{
+		Codec: mirCodec,
+		Type:  reflect.TypeOf((*NewCharacterSuccess)(nil)).Elem(),
+		ID:    NEW_CHARACTER_SUCCESS,
+	})
+	cellnet.RegisterMessageMeta(&cellnet.MessageMeta{
+		Codec: mirCodec,
+		Type:  reflect.TypeOf((*DeleteCharacter)(nil)).Elem(),
+		ID:    DELETE_CHARACTER,
+	})
+	cellnet.RegisterMessageMeta(&cellnet.MessageMeta{
+		Codec: mirCodec,
+		Type:  reflect.TypeOf((*DeleteCharacterSuccess)(nil)).Elem(),
+		ID:    DELETE_CHARACTER_SUCCESS,
+	})
+	cellnet.RegisterMessageMeta(&cellnet.MessageMeta{
+		Codec: mirCodec,
+		Type:  reflect.TypeOf((*StartGame)(nil)).Elem(),
+		ID:    START_GAME,
 	})
 }
