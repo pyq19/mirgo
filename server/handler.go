0753f1f2670c3528308e8f8129ddf76ffdfbc762
pyq <pyq225@gmail.com>
2019-12-07 13:08:53 +0800

    update

---
+++ server/handler.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"github.com/davyxu/cellnet"
+	"github.com/yenkeia/mirgo/proto/client"
+	"github.com/yenkeia/mirgo/proto/server"
+)
+
+func eventHandler(ev cellnet.Event) {
+
+	switch msg := ev.Message().(type) {
+
+	// 有新的连接
+	case *cellnet.SessionAccepted:
+		connected := server.Connected{}
+		ev.Session().Send(&connected)
+
+	// 有连接断开
+	case *cellnet.SessionClosed:
+		log.Debugln("session closed: ", ev.Session().ID())
+
+	case *client.ClientVersion:
+		clientVersion := server.ClientVersion{Result: 1}
+		ev.Session().Send(&clientVersion)
+
+	case *client.KeepAlive:
+		keepAlive := server.KeepAlive{Time: 0}
+		ev.Session().Send(keepAlive)
+
+	default:
+		log.Debugln(msg)
+	}
+}
--- server/smain.go
+++ server/smain.go
@@ -3,12 +3,9 @@ package main
 import (
 	"github.com/davyxu/cellnet"
 	"github.com/davyxu/cellnet/peer"
+	_ "github.com/davyxu/cellnet/peer/tcp"
 	"github.com/davyxu/cellnet/proc"
 	"github.com/davyxu/golog"
-	"github.com/yenkeia/mirgo/proto/client"
-	"github.com/yenkeia/mirgo/proto/server"
-
-	_ "github.com/davyxu/cellnet/peer/tcp"
 	_ "github.com/yenkeia/mirgo/proc/mirtcp"
 )
 
@@ -21,31 +18,7 @@ func main() {
 	addr := "0.0.0.0:7000"
 	p := peer.NewGenericPeer("tcp.Acceptor", "server", addr, queue)
 
-	proc.BindProcessorHandler(p, "mir.server.tcp", func(ev cellnet.Event) {
-
-		switch msg := ev.Message().(type) {
-
-		// 有新的连接
-		case *cellnet.SessionAccepted:
-			connected := server.Connected{}
-			ev.Session().Send(&connected)
-
-		// 有连接断开
-		case *cellnet.SessionClosed:
-			log.Debugln("session closed: ", ev.Session().ID())
-
-		case *client.ClientVersion:
-			clientVersion := server.ClientVersion{Result: 1}
-			ev.Session().Send(&clientVersion)
-
-		case *client.KeepAlive:
-			keepAlive := server.KeepAlive{Time: 0}
-			ev.Session().Send(keepAlive)
-
-		default:
-			log.Debugln(msg)
-		}
-	})
+	proc.BindProcessorHandler(p, "mir.server.tcp", eventHandler)
 
 	// 开始侦听
 	p.Start()
