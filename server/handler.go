59be07e402abe345d3163236da183b998406af59
pyq <pyq225@gmail.com>
2019-12-08 19:02:47 +0800

    update

--- server/handler.go
+++ server/handler.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/davyxu/cellnet"
 	"github.com/davyxu/golog"
+	"github.com/yenkeia/mirgo/codec/mircodec"
 	"github.com/yenkeia/mirgo/common"
 	"github.com/yenkeia/mirgo/proto/client"
 	"github.com/yenkeia/mirgo/proto/server"
@@ -64,6 +65,79 @@ func (g *Game) EventHandler(ev cellnet.Event) {
 		res.CharInfo.Name = msg.Name
 		res.CharInfo.Class = msg.Class
 		res.CharInfo.Gender = msg.Gender
+		ev.Session().Send(res)
+
+	// TODO
+	case *client.StartGame:
+		// TODO SetConcentration
+
+		// StartGame
+		sg := new(server.StartGame)
+		sg.Resolution = common.Down
+		sg.Result = 4
+		ev.Session().Send(sg)
+
+		// MapInformation
+		bytes := []byte{
+			1, 48,
+			14, 66, 105, 99, 104, 111, 110, 80, 114, 111, 118, 105, 110, 99, 101,
+			101, 0,
+			135, 0,
+			0, 0,
+			0,
+			0,
+			0}
+		r := new(server.MapInformation)
+		codec := new(mircodec.MirCodec)
+		if err := codec.Decode(bytes, r); err != nil {
+			panic(err)
+		}
+		ev.Session().Send(r)
+
+		// UserInformation
+		bytes = []byte{
+			128, 3, 1, 0, // ObjectID
+			1, 0, 0, 0, // RealId
+			10, 104, 101, 108, 108, 111, 119, 111, 114, 108, 100,
+			0,                  // GuildName
+			0,                  // GuildRank
+			255, 255, 255, 255, // Color	uint32	[4]byte{225, 225, 225, 225}
+			0,    // MirClass
+			0,    // MirGender
+			1, 0, // Level ushort uint16
+			28, 1, 0, 0, // Point.X uint32
+			97, 2, 0, 0, // Point.Y uint32
+			6,
+			5,
+			17, 0,
+			14, 0,
+			0, 0, 0, 0, 0, 0, 0, 0, // Experience long int42
+			100, 0, 0, 0, 0, 0, 0, 0, // MaxExperience
+			0, // LevelEffects
+			1, 46,
+			0, 0, 0, 1, 3, 0, 0, 0,
+			0, 0, 0, 0,
+			146, 2,
+			0, 0,
+			0, 0, 0, 0,
+			1, 0, 0, 0,
+			0, // AC
+			0,
+			0,
+			0,
+			0,
+			0,
+			0,
+			0,
+			0,
+			0,
+			0,                  // Luck
+			255, 255, 255, 255, // SoulBoundId
+			0,
+			0,
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 255, 255, 255, 0, 0, 0, 0, 0, 0, 0, 1, 6, 0, 0, 0, 0, 0, 0, 0, 235, 4, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 255, 255, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 255, 255, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 14, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 221, 0, 0, 0, 160, 15, 160, 15, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 255, 255, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 255, 255, 255, 0, 0, 1, 2, 0, 0, 0, 0, 0, 0, 0, 61, 1, 0, 0, 136, 19, 136, 19, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 255, 255, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 255, 255, 255, 0, 0, 0, 1, 4, 0, 0, 0, 0, 0, 0, 0, 210, 2, 0, 0, 49, 31, 64, 31, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 255, 255, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 255, 255, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 40, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 99, 0}
+		res := new(server.UserInformation)
+		res.Bytes = bytes
 		ev.Session().Send(res)
 
 	default:
