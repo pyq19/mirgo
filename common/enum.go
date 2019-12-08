d5ab06510c2485b0d1d129439c3d289045918ac2
pyq <pyq225@gmail.com>
2019-12-08 18:39:47 +0800

    update
    Former-commit-id: 963969f998d35a71c7c1e8aed0447431ba74fc3b

--- common/enum.go
+++ common/enum.go
@@ -26,3 +26,16 @@ const (
 	Evening              = 3
 	Night                = 4
 )
+
+type MirDirection uint8
+
+const (
+	Up        MirDirection = 0
+	UpRight                = 1
+	Right                  = 2
+	DownRight              = 3
+	Down                   = 4
+	DownLeft               = 5
+	Left                   = 6
+	UpLeft                 = 7
+)
