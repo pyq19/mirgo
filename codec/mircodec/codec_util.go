921a9391dea18bcd9de249629efdef6e1069fdfb
pyq <pyq225@gmail.com>
2019-12-10 19:51:10 +0800

    update

--- codec/mircodec/codec_util.go
+++ codec/mircodec/codec_util.go
@@ -5,88 +5,92 @@ import (
 	"reflect"
 )
 
-// encode 把结构体编码(序列化)成字节数组
 func encode(obj interface{}) (bytes []byte, err error) {
 	v := reflect.ValueOf(obj)
 	if v.Kind() == reflect.Ptr {
 		v = v.Elem()
 	}
 	for i := 0; i < v.NumField(); i++ {
-		f := v.Field(i)
-		// t.Log(fieldType.Name, fieldType.Type.Kind())
-		switch f.Kind() {
-		case reflect.Struct:
-			vv := f.Addr().Interface()
-			data, err := encode(vv)
-			if err != nil {
-				return bytes, err
-			}
-			bytes = append(bytes, data...)
-		case reflect.Bool:
-			vv := f.Interface().(bool)
-			b := byte(0)
-			if vv == true {
-				b = 1
-			}
-			bytes = append(bytes, b)
-		case reflect.String:
-			vv := f.Interface().(string)
-			sb := StringToBytes(vv)
-			bytes = append(bytes, sb...)
-		case reflect.Int8:
-			vv := uint8(f.Interface().(int8))
-			bytes = append(bytes, vv)
-		case reflect.Int16:
-			vv := uint16(f.Interface().(int16))
-			bytes = append(bytes, Uint16ToBytes(vv)...)
-		case reflect.Int32:
-			vv := uint32(f.Interface().(int32))
-			bytes = append(bytes, Uint32ToBytes(vv)...)
-		case reflect.Int64:
-			vv := uint64(f.Interface().(int64))
-			bytes = append(bytes, Uint64ToBytes(vv)...)
-		case reflect.Uint8:
-			switch vv := f.Interface().(type) {
-			case uint8:
-				bytes = append(bytes, vv)
-			default:
-				vvv := reflect.ValueOf(vv).Uint()
-				bytes = append(bytes, uint8(vvv))
-			}
-		case reflect.Uint16:
-			vv := f.Interface().(uint16)
-			bytes = append(bytes, Uint16ToBytes(vv)...)
-		case reflect.Uint32:
-			vv := f.Interface().(uint32)
-			bytes = append(bytes, Uint32ToBytes(vv)...)
-		case reflect.Uint64:
-			vv := f.Interface().(uint64)
-			bytes = append(bytes, Uint64ToBytes(vv)...)
-		case reflect.Slice:
-			// FIXME 还有别的类型可能会报错
-			switch vv := f.Interface().(type) {
-			case []uint8:
-				bytes = append(bytes, vv...)
-			default:
-				vvv := reflect.ValueOf(vv)
-				l := vvv.Len()
-				slice := vvv.Slice(0, l)
-				bytes = append(bytes, Uint32ToBytes(uint32(l))...)
-				for i := 0; i < l; i++ {
-					b, err := encode(slice.Index(i).Interface())
-					if err != nil {
-						panic(err)
-					}
-					bytes = append(bytes, b...)
-				}
-			}
-		default:
-			log.Errorln("编码错误")
-			return bytes, errors.New("编码错误")
+		encodeBytes, err := encodeValue(v.Field(i))
+		if err != nil {
+			return bytes, err
 		}
+		bytes = append(bytes, encodeBytes...)
 	}
-	if bytes == nil {
-		bytes = []byte{}
+	return
+}
+
+// encode 把结构体编码(序列化)成字节数组
+func encodeValue(v reflect.Value) (bytes []byte, err error) {
+	switch v.Kind() {
+	case reflect.Struct:
+		vv := v.Addr().Interface()
+		data, err := encode(vv)
+		if err != nil {
+			return bytes, err
+		}
+		bytes = append(bytes, data...)
+	case reflect.Bool:
+		vv := v.Interface().(bool)
+		b := byte(0)
+		if vv == true {
+			b = 1
+		}
+		bytes = append(bytes, b)
+	case reflect.String:
+		vv := v.Interface().(string)
+		sb := StringToBytes(vv)
+		bytes = append(bytes, sb...)
+	case reflect.Int8:
+		vv := uint8(v.Interface().(int8))
+		bytes = append(bytes, vv)
+	case reflect.Int16:
+		vv := uint16(v.Interface().(int16))
+		bytes = append(bytes, Uint16ToBytes(vv)...)
+	case reflect.Int32:
+		vv := uint32(v.Interface().(int32))
+		bytes = append(bytes, Uint32ToBytes(vv)...)
+	case reflect.Int64:
+		vv := uint64(v.Interface().(int64))
+		bytes = append(bytes, Uint64ToBytes(vv)...)
+	case reflect.Uint8:
+		switch vv := v.Interface().(type) {
+		case uint8:
+			bytes = append(bytes, vv)
+		default:
+			vvv := reflect.ValueOf(vv).Uint()
+			bytes = append(bytes, uint8(vvv))
+		}
+	case reflect.Uint16:
+		vv := v.Interface().(uint16)
+		bytes = append(bytes, Uint16ToBytes(vv)...)
+	case reflect.Uint32:
+		vv := v.Interface().(uint32)
+		bytes = append(bytes, Uint32ToBytes(vv)...)
+	case reflect.Uint64:
+		vv := v.Interface().(uint64)
+		bytes = append(bytes, Uint64ToBytes(vv)...)
+	case reflect.Slice:
+		// FIXME 还有别的类型可能会报错
+		switch vv := v.Interface().(type) {
+		case []uint8:
+			bytes = append(bytes, vv...)
+		default:
+			vvv := reflect.ValueOf(vv)
+			l := vvv.Len()
+			slice := vvv.Slice(0, l)
+			bytes = append(bytes, Uint32ToBytes(uint32(l))...)
+			for i := 0; i < l; i++ {
+				b, err := encode(slice.Index(i).Interface())
+				if err != nil {
+					panic(err)
+				}
+				bytes = append(bytes, b...)
+			}
+		}
+	default:
+		log.Errorln("编码错误")
+		return bytes, errors.New("编码错误")
 	}
 	return bytes, nil
 }
