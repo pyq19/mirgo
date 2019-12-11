ba369d7296cfec8382206d191e13fc458574dc31
pyq <pyq225@gmail.com>
2019-12-12 02:12:39 +0800

    update
    Former-commit-id: ae610abd83391e46355050b8ec8069af18c6a57e

--- server/env.go
+++ server/env.go
@@ -1,9 +1,52 @@
 package main
 
+import (
+	"github.com/davyxu/cellnet"
+	"github.com/yenkeia/mirgo/common"
+)
+
 type Environ struct {
+	Game          *Game
+	Basic         common.Basic
+	Players       *[]Player
+	Maps          *[]Map
+	Monsters      *[]Monster
+	NPCs          *[]NPC
+	GameShopItems *[]common.GameShopItem
+	ItemInfos     *[]common.ItemInfo
+	MagicInfos    *[]common.MagicInfo
+	MovementInfos *[]common.MovementInfo
+	QuestInfos    *[]common.QuestInfo
 }
 
-func (g *Game) NewEnv() (env *Environ) {
+type Map struct {
+	Info      *common.MapInfo
+	SafeZones *[]common.SafeZoneInfo
+}
 
+type MapObject interface {
+}
+
+type Player struct {
+	MapObject
+	Session   *cellnet.Session
+	Character *common.Character
+	UserItems *[]common.UserItem
+}
+
+type Monster struct {
+	MapObject
+	Info *common.MonsterInfo
+}
+
+type NPC struct {
+	MapObject
+	Info *common.NpcInfo
+}
+
+// TODO
+func (g *Game) NewEnviron() (env *Environ) {
+	env = new(Environ)
+	env.Game = g
 	return
 }
