68a7538ca8ba22744310c6564769458f58266ca4
pyq <pyq225@gmail.com>
2019-12-12 01:25:36 +0800

    update

--- common/struct_orm_test.go
+++ common/struct_orm_test.go
@@ -24,37 +24,37 @@ func TestDB(t *testing.T) {
 	t.Log(gameShopItem.GoldPrice, gameShopItem.Id)
 
 	var magicInfo MagicInfo
-	db.Table("magic_info").Where("name = ?", "Fencing").Find(&magicInfo)
+	db.Table("magic").Where("name = ?", "Fencing").Find(&magicInfo)
 	t.Log(magicInfo.Name, magicInfo.Icon)
 
 	var mapInfo MapInfo
-	db.Table("map_info").Where("id = ?", 1).Find(&mapInfo)
+	db.Table("map").Where("id = ?", 1).Find(&mapInfo)
 	t.Log(mapInfo.Title)
 
 	//var mineZone com.MineZone
 	//db.Table("mine_zone").Where("map_index = ?", )
 
 	var monsterInfo MonsterInfo
-	db.Table("monster_info").Where("id = ?", 1).Find(&monsterInfo)
+	db.Table("monster").Where("id = ?", 1).Find(&monsterInfo)
 	t.Log(monsterInfo.Name)
 
 	var movementInfo MovementInfo
-	db.Table("movement_info").Where("map_id = ?", 2).First(&movementInfo)
+	db.Table("movement").Where("map_id = ?", 2).First(&movementInfo)
 	t.Log(movementInfo.MapId, movementInfo.ConquestIndex, movementInfo.DestinationX, movementInfo.DestinationY)
 
 	var npcInfo NpcInfo
-	db.Table("npc_info").Where("id = ?", 1).Find(&npcInfo)
+	db.Table("npc").Where("id = ?", 1).Find(&npcInfo)
 	t.Log(npcInfo.Filename)
 
 	var questInfo QuestInfo
-	db.Table("quest_info").Where("id = ?", 1).Find(&questInfo)
+	db.Table("quest").Where("id = ?", 1).Find(&questInfo)
 	t.Log(questInfo.Name)
 
 	var respawnInfo RespawnInfo
-	db.Table("respawn_info").Where("location_x = ?", 350).Find(&respawnInfo)
+	db.Table("respawn").Where("location_x = ?", 350).Find(&respawnInfo)
 	t.Log(respawnInfo.MapId, respawnInfo.RespawnIndex, respawnInfo.Count)
 
 	var safeZoneInfo SafeZoneInfo
-	db.Table("safe_zone_info").Where("map_id = ?", 1).Find(&safeZoneInfo)
+	db.Table("safe_zone").Where("map_id = ?", 1).Find(&safeZoneInfo)
 	t.Log(safeZoneInfo.MapId, safeZoneInfo.LocationX, safeZoneInfo.LocationY)
 }
