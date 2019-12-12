d3e2bea9a6968b7d97bb9541eda3f107bbe01122
pyq <pyq225@gmail.com>
2019-12-13 00:38:28 +0800

    update

--- server/env.go
+++ server/env.go
@@ -6,42 +6,29 @@ import (
 )
 
 type Environ struct {
-	Game          *Game
+	Game    *Game
+	Players *[]Player // 总玩家
+}
+
+type ServerDB struct {
 	Basic         common.Basic
-	Players       *[]Player
-	Maps          *[]Map
-	Monsters      *[]Monster
-	NPCs          *[]NPC
-	GameShopItems *[]common.GameShopItem
-	ItemInfos     *[]common.ItemInfo
-	MagicInfos    *[]common.MagicInfo
-	MovementInfos *[]common.MovementInfo
-	QuestInfos    *[]common.QuestInfo
-}
-
-type Map struct {
-	Info      *common.MapInfo
-	SafeZones *[]common.SafeZoneInfo
-}
-
-type MapObject interface {
+	GameShopItems []common.GameShopItem
+	ItemInfos     []common.ItemInfo
+	MagicInfos    []common.MagicInfo
+	MapInfos      []common.MapInfo
+	MonsterInfos  []common.MonsterInfo
+	MovementInfos []common.MovementInfo
+	NpcInfos      []common.NpcInfo
+	QuestInfos    []common.QuestInfo
+	RespawnInfos  []common.RespawnInfo
+	SafeZoneInfos []common.SafeZoneInfo
 }
 
 type Player struct {
-	MapObject
 	Session   *cellnet.Session
 	Character *common.Character
+	Magics    *[]common.MagicInfo
 	UserItems *[]common.UserItem
-}
-
-type Monster struct {
-	MapObject
-	Info *common.MonsterInfo
-}
-
-type NPC struct {
-	MapObject
-	Info *common.NpcInfo
 }
 
 // TODO
