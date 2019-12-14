b2ec388968524ada7c71826decc612b94750d122
pyq <pyq225@gmail.com>
2019-12-14 14:49:40 +0800

    update
    Former-commit-id: 19bce6f3824309b79cb813039fb01a2733614b20

--- server/env.go
+++ server/env.go
@@ -11,6 +11,55 @@ type Environ struct {
 	Players *[]Player // 总玩家
 }
 
+func (e Environ) InitGameDB() {
+	db := e.Game.DB
+	gdb := e.GameDB
+
+	b := new(common.Basic)
+	db.Table("basic").Find(b)
+	gdb.Basic = *b
+
+	gsi := make([]common.GameShopItem, 106)
+	db.Table("game_shop_item").Find(&gsi)
+	gdb.GameShopItems = gsi
+
+	ii := make([]common.ItemInfo, 1346)
+	db.Table("item").Find(&ii)
+	gdb.ItemInfos = ii
+
+	mi := make([]common.MagicInfo, 105)
+	db.Table("magic").Find(&mi)
+	gdb.MagicInfos = mi
+
+	mp := make([]common.MapInfo, 386)
+	db.Table("map").Find(&mp)
+	gdb.MapInfos = mp
+
+	ms := make([]common.MonsterInfo, 506)
+	db.Table("monster").Find(&ms)
+	gdb.MonsterInfos = ms
+
+	mm := make([]common.MovementInfo, 1837)
+	db.Table("movement").Find(&mm)
+	gdb.MovementInfos = mm
+
+	ni := make([]common.NpcInfo, 293)
+	db.Table("npc").Find(&ni)
+	gdb.NpcInfos = ni
+
+	qi := make([]common.QuestInfo, 157)
+	db.Table("quest").Find(&qi)
+	gdb.QuestInfos = qi
+
+	ri := make([]common.RespawnInfo, 5931)
+	db.Table("respawn").Find(&ri)
+	gdb.RespawnInfos = ri
+
+	si := make([]common.SafeZoneInfo, 19)
+	db.Table("safe_zone").Find(&si)
+	gdb.SafeZoneInfos = si
+}
+
 type GameDB struct {
 	Basic         common.Basic
 	GameShopItems []common.GameShopItem
@@ -25,11 +74,6 @@ type GameDB struct {
 	SafeZoneInfos []common.SafeZoneInfo
 }
 
-// TODO
-func (d GameDB) Init() {
-
-}
-
 type Player struct {
 	Session   *cellnet.Session
 	Character *common.Character
@@ -40,11 +84,9 @@ type Player struct {
 func (g *Game) NewEnviron() (env *Environ) {
 	env = new(Environ)
 	env.Game = g
-
-	gameDB := new(GameDB)
-	gameDB.Init()
-
-	players := make([]Player, 0)
+	env.GameDB = new(GameDB)
+	env.InitGameDB()
+	players := make([]Player, 0, 50)
 	env.Players = &players
 	return
 }
