19bce6f3824309b79cb813039fb01a2733614b20
pyq <pyq225@gmail.com>
2019-12-14 14:49:40 +0800

    update

--- server/game.go
+++ server/game.go
@@ -31,22 +31,11 @@ func NewGame(conf Config) *Game {
 }
 
 func (g *Game) ServerStart() {
-
 	queue := cellnet.NewEventQueue()
-
 	p := peer.NewGenericPeer("tcp.Acceptor", "server", g.Conf.Addr, queue)
-
 	g.Peer = &p
-
 	proc.BindProcessorHandler(p, "mir.server.tcp", g.HandleEvent)
-
-	// 开始侦听
-	p.Start()
-
-	// 事件队列开始循环
-	queue.StartLoop()
-
-	// 阻塞等待事件队列结束退出( 在另外的goroutine调用queue.StopLoop() )
-	queue.Wait()
-
+	p.Start()         // 开始侦听
+	queue.StartLoop() // 事件队列开始循环
+	queue.Wait()      // 阻塞等待事件队列结束退出( 在另外的goroutine调用queue.StopLoop() )
 }
