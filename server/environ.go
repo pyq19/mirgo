b2da3906e5c0fb8b77eb468bf3cb502e7ca4729b
pyq <pyq225@gmail.com>
2019-12-16 19:40:53 +0800

    update
    Former-commit-id: 24397b088ba52eb8bfa15192fc70578f614a4326

--- server/environ.go
+++ server/environ.go
@@ -9,7 +9,6 @@ import (
 type Environ struct {
 	Game               *Game
 	GameDB             *GameDB
-	Players            []Player // 总玩家
 	SessionIDPlayerMap map[int64]Player
 	// CommonEventChan  chan interface{} // 系统事件
 	// PlayerEventChan  chan interface{} // 玩家事件
@@ -56,8 +55,6 @@ func (g *Game) NewEnviron() (env *Environ) {
 	env.LoadGameDB()
 	env.InitMaps()
 	env.InitMonsters()
-	players := make([]Player, 0, 50)
-	env.Players = players
 	env.SessionIDPlayerMap = make(map[int64]Player)
 	return
 }
