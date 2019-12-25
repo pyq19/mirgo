7ee1ce515f0f2167da1f5c18710329e6b3c47802
pyq <pyq225@gmail.com>
2019-12-25 17:32:54 +0800

    update
    Former-commit-id: a412e1c400530dc0e5ea8d2606ca055620f6643c

--- server/map.go
+++ server/map.go
@@ -7,17 +7,17 @@ import (
 
 // Map ...
 type Map struct {
+	Env               *Environ
 	Id                int
 	Width             uint16 // 测试用
 	Height            uint16 // 测试用
 	Info              *common.MapInfo
 	Cells             []Cell
 	CoordinateCellMap *sync.Map // map[string]*Cell
-	AOIManager        *AOIManager
-	//WalkableCells     []Cell
+	AOI               *AOIManager
 }
 
-func (m *Map) GetCell(coordinate string) *Cell {
+func (m *Map) getCell(coordinate string) *Cell {
 	v, ok := m.CoordinateCellMap.Load(coordinate)
 	if !ok {
 		return nil
@@ -26,7 +26,15 @@ func (m *Map) GetCell(coordinate string) *Cell {
 }
 
 func (m *Map) AddObject(obj interface{}) {
-
+	switch o := obj.(type) {
+	case *Player:
+		p := common.NewPoint(int(o.Character.CurrentLocationX), int(o.Character.CurrentLocationY))
+		c := m.getCell(p.Coordinate())
+		c.SetPlayer(o)
+	case *Respawn:
+		m.getCell(o.Point().Coordinate()).SetRespawn(o)
+	case *NPC:
+	}
 }
 
 func (m *Map) DeleteObject(obj interface{}) {
