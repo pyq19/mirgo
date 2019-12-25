03a38501543c19c0752090e0501accbff1f4fbef
pyq <pyq225@gmail.com>
2019-12-25 15:06:37 +0800

    update
    Former-commit-id: a3af89607d88f7a579147835bc58e871cc3d559d

--- server/grid.go
+++ server/grid.go
@@ -1,62 +1,58 @@
 package main
 
 import (
-	"fmt"
 	"sync"
 )
 
 // Grid 一个地图中的格子类
 type Grid struct {
-	GID       int          // 格子ID
-	MinX      int          // 格子左边界坐标
-	MaxX      int          // 格子右边界坐标
-	MinY      int          // 格子上边界坐标
-	MaxY      int          // 格子下边界坐标
-	playerIDs map[string]bool // 当前格子内的玩家或者物体成员ID
-	pIDLock   sync.RWMutex // playerIDs的保护map的锁
+	GID     int       // 格子ID
+	MinX    int       // 格子左边界坐标
+	MaxX    int       // 格子右边界坐标
+	MinY    int       // 格子上边界坐标
+	MaxY    int       // 格子下边界坐标
+	players *sync.Map // 当前格子内的玩家 key=playerID  value=*player
 }
 
 // NewGrid 初始化一个格子
 func NewGrid(gID, minX, maxX, minY, maxY int) *Grid {
 	return &Grid{
-		GID:       gID,
-		MinX:      minX,
-		MaxX:      maxX,
-		MinY:      minY,
-		MaxY:      maxY,
-		playerIDs: make(map[string]bool),
+		GID:     gID,
+		MinX:    minX,
+		MaxX:    maxX,
+		MinY:    minY,
+		MaxY:    maxY,
+		players: new(sync.Map),
 	}
 }
 
 // Add 向当前格子中添加一个玩家
-func (g *Grid) Add(playerID string) {
-	g.pIDLock.Lock()
-	defer g.pIDLock.Unlock()
-
-	g.playerIDs[playerID] = true
+func (g *Grid) Add(p *Player) {
+	g.players.Store(p.ID, p)
 }
 
 // Remove 从格子中删除一个玩家
-func (g *Grid) Remove(playerID string) {
-	g.pIDLock.Lock()
-	defer g.pIDLock.Unlock()
-
-	delete(g.playerIDs, playerID)
+func (g *Grid) Remove(p *Player) {
+	v, ok := g.players.Load(p.ID)
+	if !ok {
+		return
+	}
+	g.players.Delete(v.(*Player).ID)
 }
 
 // GetPlayerID 得到当前格子中所有的玩家
-func (g *Grid) GetPlayerID() (playerIDs []string) {
-	g.pIDLock.RLock()
-	defer g.pIDLock.RUnlock()
-
-	for k, _ := range g.playerIDs {
-		playerIDs = append(playerIDs, k)
-	}
+func (g *Grid) GetAllPlayer() (players []*Player) {
+	g.players.Range(func(k, v interface{}) bool {
+		players = append(players, v.(*Player))
+		return true
+	})
 	return
 }
 
+/*
 // String 打印信息方法
 func (g *Grid) String() string {
 	return fmt.Sprintf("Grid id: %d, minX:%d, maxX:%d, minY:%d, maxY:%d, playerIDs:%v",
 		g.GID, g.MinX, g.MaxX, g.MinY, g.MaxY, g.playerIDs)
 }
+*/
