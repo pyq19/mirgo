a3af89607d88f7a579147835bc58e871cc3d559d
pyq <pyq225@gmail.com>
2019-12-25 15:06:37 +0800

    update

--- server/aoi.go
+++ server/aoi.go
@@ -1,18 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"sync"
+)
 
 // Zinx应用-MMO游戏案例-(2)AOI兴趣点算法 https://www.jianshu.com/p/e5b5db9fa6fe
 
 // AOIManager 每一张地图对应一个 AOI 管理模块
 type AOIManager struct {
-	MinX  int           // 区域左边界坐标
-	MaxX  int           // 区域右边界坐标
-	CntsX int           // x方向格子的数量
-	MinY  int           // 区域上边界坐标
-	MaxY  int           // 区域下边界坐标
-	CntsY int           // y方向的格子数量
-	grids map[int]*Grid // 当前区域中都有哪些格子，key=格子ID， value=格子对象
+	MinX  int       // 区域左边界坐标
+	MaxX  int       // 区域右边界坐标
+	CntsX int       // x方向格子的数量
+	MinY  int       // 区域上边界坐标
+	MaxY  int       // 区域下边界坐标
+	CntsY int       // y方向的格子数量
+	grids *sync.Map // map[int]*Grid 当前区域中都有哪些格子，key=格子ID， value=格子对象
 }
 
 // NewAOIManager 初始化一个AOI区域
@@ -24,7 +26,7 @@ func NewAOIManager(minX, maxX, cntsX, minY, maxY, cntsY int) *AOIManager {
 		MinY:  minY,
 		MaxY:  maxY,
 		CntsY: cntsY,
-		grids: make(map[int]*Grid),
+		grids: new(sync.Map),
 	}
 
 	// 给AOI初始化区域中所有的格子
@@ -32,14 +34,15 @@ func NewAOIManager(minX, maxX, cntsX, minY, maxY, cntsY int) *AOIManager {
 		for x := 0; x < cntsX; x++ {
 			// 计算格子ID
 			// 格子编号：id = idy *nx + idx  (利用格子坐标得到格子编号)
-			gid := y*cntsX + x
+			gridID := y*cntsX + x
 
 			// 初始化一个格子放在AOI中的map里，key是当前格子的ID
-			aoiMgr.grids[gid] = NewGrid(gid,
+			grid := NewGrid(gridID,
 				aoiMgr.MinX+x*aoiMgr.gridWidth(),
 				aoiMgr.MinX+(x+1)*aoiMgr.gridWidth(),
 				aoiMgr.MinY+y*aoiMgr.gridLength(),
 				aoiMgr.MinY+(y+1)*aoiMgr.gridLength())
+			aoiMgr.grids.Store(gridID, grid)
 		}
 	}
 
@@ -56,6 +59,7 @@ func (m *AOIManager) gridLength() int {
 	return (m.MaxY - m.MinY) / m.CntsY
 }
 
+/*
 // String 打印信息方法
 func (m *AOIManager) String() string {
 	s := fmt.Sprintf("AOIManagr:\nminX:%d, maxX:%d, cntsX:%d, minY:%d, maxY:%d, cntsY:%d\n Grids in AOI Manager:\n",
@@ -65,3 +69,4 @@ func (m *AOIManager) String() string {
 	}
 	return s
 }
+*/
---
+++ server/grid_test.go
@@ -0,0 +1,19 @@
+package main
+
+import "testing"
+
+func TestGrid_GetPlayerID(t *testing.T) {
+	grid := NewGrid(1, 1, 1, 1, 1)
+
+	p1 := new(Player)
+	p1.ID = "p1p1p1"
+
+	p2 := new(Player)
+	p2.ID = "p2p2p2"
+
+	grid.Add(p1)
+	grid.Add(p2)
+
+	players := grid.GetAllPlayer()
+	t.Log(players[0].ID, players[1].ID)
+}
