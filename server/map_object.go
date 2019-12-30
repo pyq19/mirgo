7214e63099b7da67ca0d43016750fd5b47f4793d
pyq <pyq225@gmail.com>
2019-12-30 13:45:05 +0800

    update
    Former-commit-id: 3401b5067a9d4fc1c71c6770c16c81dd053ce7d4

--- server/map_object.go
+++ server/map_object.go
@@ -8,4 +8,17 @@ type MapObject struct {
 	Map              *Map
 	CurrentLocation  *common.Point
 	CurrentDirection common.MirDirection
+	Level            uint16
+	Health           uint32 // 当前生命值
+	MaxMaxHealth     uint32 // 最大生命值
+	MinAC            uint16 // 物理防御力
+	MaxAC            uint16
+	MinMAC           uint16 // 魔法防御力
+	MaxMAC           uint16
+	MinDC            uint16 // 攻击力
+	MaxDC            uint16
+	MinMC            uint16 // 魔法力
+	MaxMC            uint16
+	MinSC            uint16 // 道术力
+	MaxSC            uint16
 }
