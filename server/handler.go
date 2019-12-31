aaf7358927f42e0029866ae4d078c9d641ea48f3
pyq <pyq225@gmail.com>
2019-12-31 11:14:24 +0800

    update

--- server/handler.go
+++ server/handler.go
@@ -683,36 +683,11 @@ func (g *Game) Turn(p *Player, msg *client.Turn) {
 }
 
 func (g *Game) Walk(p *Player, msg *client.Walk) {
-	c := p.Map.GetCell(p.Coordinate())
-	nc := p.Map.GetNextCell(c, msg.Direction, 1)
-	if nc == nil || !nc.IsValid() {
-		p.Send(&server.UserLocation{
-			Location:  p.Point(),
-			Direction: p.CurrentDirection,
-		})
-		return
-	}
-	c.SetObject(nil)
-	nc.SetObject(p)
-	// TODO change AOI
-	p.Walk(msg.Direction, nc.Point())
+	p.Walk(msg.Direction)
 }
 
 func (g *Game) Run(p *Player, msg *client.Run) {
-	c := p.Map.GetCell(p.Coordinate())
-	nc1 := p.Map.GetNextCell(c, msg.Direction, 1)
-	nc2 := p.Map.GetNextCell(c, msg.Direction, 2)
-	if nc1 == nil || nc2 == nil || !nc1.IsValid() || !nc2.IsValid() {
-		p.Send(&server.UserLocation{
-			Location:  p.Point(),
-			Direction: p.CurrentDirection,
-		})
-		return
-	}
-	c.SetObject(nil)
-	nc2.SetObject(p)
-	// TODO change AOI
-	p.Run(msg.Direction, nc2.Point())
+	p.Run(msg.Direction)
 }
 
 func (g *Game) Chat(p *Player, msg *client.Chat) {
