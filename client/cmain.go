c73d60ce12dde55c17053f0284c1506de29846cb
pyq <pyq225@gmail.com>
2020-01-09 15:14:03 +0800

    update
    Former-commit-id: 27b4011e2b6548fd55df90f679c408bdb96699de

--- client/cmain.go
+++ client/cmain.go
@@ -8,6 +8,7 @@ import (
 	"github.com/davyxu/cellnet/proc"
 	"github.com/davyxu/golog"
 	_ "github.com/yenkeia/mirgo/codec/mircodec"
+	"github.com/yenkeia/mirgo/common"
 	_ "github.com/yenkeia/mirgo/proc/mirtcp"
 	"github.com/yenkeia/mirgo/proto/client"
 	"github.com/yenkeia/mirgo/proto/server"
@@ -18,14 +19,53 @@ import (
 
 var log = golog.New("client")
 
+func readConsole(callback func(string)) {
+	for {
+		// 从标准输入读取字符串，以\n为分割
+		text, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			break
+		}
+		// 去掉读入内容的空白符
+		text = strings.TrimSpace(text)
+		callback(text)
+	}
+}
+
+func parse(str string) interface{} {
+	args := strings.Split(str, " ")
+	switch args[0] {
+	case "CLIENT_VERSION":
+		return &client.ClientVersion{VersionHash: []uint8{16, 0, 0, 0, 86, 92, 129, 20, 102, 64, 159, 148, 125, 97, 112, 85, 237, 250, 133, 162}}
+	case "KEEP_ALIVE":
+		return &client.KeepAlive{Time: 0}
+	case "LOGIN": // 5
+		return &client.Login{
+			AccountID: args[1],
+			Password:  args[2],
+		}
+	case "NEW_CHARACTER":
+		return &client.NewCharacter{
+			Name:   args[1],
+			Gender: common.MirGenderMale,
+			Class:  common.MirClassTaoist,
+		}
+	case "START_GAME": // 8
+		i, _ := strconv.Atoi(args[1])
+		return &client.StartGame{CharacterIndex: int16(i)}
+	case "DELETE_CHARACTER":
+		i, _ := strconv.Atoi(args[1])
+		return &client.DeleteCharacter{CharacterIndex: int16(i)}
+	default:
+		return nil
+	}
+}
+
 func main() {
-
 	queue := cellnet.NewEventQueue()
-
 	//addr := "192.168.31.242:7000"
 	addr := "127.0.0.1:7000"
 	p := peer.NewGenericPeer("tcp.Connector", "client", addr, queue)
-
 	proc.BindProcessorHandler(p, "mir.client.tcp", func(ev cellnet.Event) {
 		switch msg := ev.Message().(type) {
 		case *cellnet.SessionConnected:
@@ -44,63 +84,15 @@ func main() {
 			log.Debugf("default: 客户端收到: %s", msg)
 		}
 	})
-
 	p.Start()
-
 	queue.StartLoop()
-
 	log.Debugln("Ready to chat!")
-
 	readConsole(func(str string) {
 		session := p.(interface{ Session() cellnet.Session }).Session()
-
-		id, _ := strconv.Atoi(str)
-		id = id + 1000
-		idStr := strconv.Itoa(id)
-		switch id {
-		case client.CLIENT_VERSION:
-			log.Debugln(idStr + " CLIENT_VERSION")
-			session.Send(&client.ClientVersion{
-				VersionHash: []uint8{16, 0, 0, 0, 86, 92, 129, 20, 102, 64, 159, 148, 125, 97, 112, 85, 237, 250, 133, 162},
-			})
-		case client.KEEP_ALIVE:
-			log.Debugln(idStr + " KEEP_ALIVE")
-			session.Send(&client.KeepAlive{
-				Time: 0,
-			})
-		case client.LOGIN: // 5
-			session.Send(&client.Login{
-				AccountID: "test",
-				Password:  "testt",
-			})
-		case client.NEW_CHARACTER:
-			log.Debugln(idStr + " NEW_CHARACTER")
-			//session.Send(&client.NewCharacter{
-			//	Name:   "test character",
-			//	Gender: common.MirGenderMale,
-			//	Class:  common.MirClassTaoist,
-			//})
-		case client.START_GAME: // 8
-			log.Debugln(idStr + " START_GAME")
-			session.Send(&client.StartGame{CharacterIndex: 1})
-		case client.DELETE_CHARACTER:
-			log.Debugln(idStr + " DELETE_CHARACTER")
-			//session.Send(&client.DeleteCharacter{CharacterIndex: 3})
-		default:
-			log.Debugln(idStr + " default")
+		msg := parse(str)
+		if msg == nil {
+			return
 		}
+		session.Send(msg)
 	})
-}
-
-func readConsole(callback func(string)) {
-	for {
-		// 从标准输入读取字符串，以\n为分割
-		text, err := bufio.NewReader(os.Stdin).ReadString('\n')
-		if err != nil {
-			break
-		}
-		// 去掉读入内容的空白符
-		text = strings.TrimSpace(text)
-		callback(text)
-	}
 }
