eb967b3941c720234ce0403f5ebdd411d4754a50
pyq <pyq225@gmail.com>
2020-01-29 14:15:13 +0800

    update
    Former-commit-id: de1e8b56063de862e59e58137a487776a95978cb

--- common/struct_file_test.go
+++ common/struct_file_test.go
@@ -16,6 +16,10 @@ func TestGetDropInfosByMonsterName(t *testing.T) {
 	gopath := os.Getenv("GOPATH")
 	monsterName := "Ancient_AxeSkeleton"
 	dropDirPath := gopath + "/src/github.com/yenkeia/mirgo/dotnettools/database/Envir/Drops/"
-	drops := GetDropInfosByMonsterName(dropDirPath, monsterName)
-	t.Log(drops)
+	drops, err := GetDropInfosByMonsterName(dropDirPath, monsterName)
+	if err != nil {
+		t.Error(err)
+	} else {
+		t.Log(drops)
+	}
 }
