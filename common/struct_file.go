7d9a2eeefc922b171d668e159b39ecdb9b39a6ce
pyq <pyq225@gmail.com>
2020-01-29 14:06:50 +0800

    add drop info
    Former-commit-id: e7ae1fa284cdd449b99596562c0edcf5c61b75ed

--- common/struct_file.go
+++ common/struct_file.go
@@ -1,17 +1,53 @@
 package common
 
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
 // DropInfo 一个 DropInfo 对象对应 database/Envir/Drops 文件夹内一个文件的一行
 type DropInfo struct {
-	Chance int
-	Gold   int
-	ItemID int // ItemInfo.ID
+	Chance   int // 1/3 中的 3，1/10 中的 10
+	Gold     int
+	ItemName string // ItemInfo.Name
 }
 
 func NewDropInfo(line string) *DropInfo {
-	return nil
+	line = strings.TrimSpace(line)
+	res := strings.Split(line, " ")
+	chance, err := strconv.Atoi(strings.Split(res[0], "/")[1])
+	if err != nil {
+		panic("NewDropInfo 格式不正确" + err.Error())
+	}
+	if len(res) == 3 {
+		// 1/10 Gold 500
+		gold, _ := strconv.Atoi(res[2])
+		return &DropInfo{Chance: chance, Gold: gold, ItemName: "Gold"}
+	} else if len(res) == 2 {
+		// 1/5 (MP)DrugLarge
+		return &DropInfo{Chance: chance, Gold: 0, ItemName: res[1]}
+	} else {
+		panic("NewDropInfo 格式不正确")
+	}
 }
 
 // GetDropInfosByMonsterName 加载怪物掉落物品
-func GetDropInfosByMonsterName(dropDirPath, monsterName string) []DropInfo {
-	return nil
+func GetDropInfosByMonsterName(dropDirPath, monsterName string) (res []DropInfo) {
+	filename := dropDirPath + monsterName + ".txt"
+	file, err := os.Open(filename)
+	defer file.Close()
+	if err != nil {
+		return nil
+	}
+	fscanner := bufio.NewScanner(file)
+	for fscanner.Scan() {
+		line := fscanner.Text()
+		if line == "" || strings.HasPrefix(line, ";") {
+			continue
+		}
+		res = append(res, *NewDropInfo(line))
+	}
+	return
 }
--- common/struct_file_test.go
+++ common/struct_file_test.go
@@ -1,11 +1,21 @@
 package common
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDropInfo(t *testing.T) {
-
+	di1 := NewDropInfo("1/10 Gold 500")
+	t.Log(di1)
+	di2 := NewDropInfo("1/5 (MP)DrugLarge")
+	t.Log(di2)
 }
 
 func TestGetDropInfosByMonsterName(t *testing.T) {
-
+	gopath := os.Getenv("GOPATH")
+	monsterName := "Ancient_AxeSkeleton"
+	dropDirPath := gopath + "/src/github.com/yenkeia/mirgo/dotnettools/database/Envir/Drops/"
+	drops := GetDropInfosByMonsterName(dropDirPath, monsterName)
+	t.Log(drops)
 }
