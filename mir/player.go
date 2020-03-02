18757df49116506ba2c14eba80487fe9ade6c1e9
pyq <pyq225@gmail.com>
2020-03-02 20:14:52 +0800

    LogOut SessionClosed 时保存游戏数据

--- mir/player.go
+++ mir/player.go
@@ -100,7 +100,6 @@ type Player struct {
 	PMode              common.PetMode
 	CallingNPC         *NPC
 	CallingNPCPage     string
-	SaveNextTime       time.Time
 }
 
 type Health struct {
@@ -424,12 +423,6 @@ func (p *Player) Process(dt time.Duration) {
 		*ch.HealNextTime = now.Add(ch.HealDuration)
 		p.ChangeHP(int(float32(p.MaxHP)*0.03) + 1)
 		p.ChangeMP(int(float32(p.MaxMP)*0.03) + 1)
-	}
-
-	// 保存玩家数据
-	if now.After(p.SaveNextTime) {
-		p.SaveNextTime = now.Add(SavePlayerDataDuration)
-		p.SaveData()
 	}
 }
 
