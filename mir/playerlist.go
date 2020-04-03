065e78bc852271327cb9d8fbf48f5c189727b5a7
pyq <pyq225@gmail.com>
2020-04-03 22:59:00 +0800

    update 组队
    Former-commit-id: cb17357065aedd79c1f8c5680c926de1aa114eb0

--- mir/playerlist.go
+++ mir/playerlist.go
@@ -1,33 +1,35 @@
 package mir
 
-import "container/list"
-
 // PlayerList 玩家列表
 type PlayerList struct {
-	List *list.List
+	List []*Player
 }
 
 // NewPlayerList 新建列表
 func NewPlayerList() *PlayerList {
-	return nil
+	return &PlayerList{List: make([]*Player, 0)}
 }
 
 // Get 获取 PlayerList 里索引为 i 的玩家
 func (l *PlayerList) Get(i int) *Player {
-	return nil
+	return l.List[i]
 }
 
 // Add 把玩家加到 PlayerList
 func (l *PlayerList) Add(p *Player) {
-
+	l.List = append(l.List, p)
 }
 
 // Count 玩家数量
 func (l *PlayerList) Count() int {
-	return l.List.Len()
+	return len(l.List)
 }
 
 // Remove 从列表里删除玩家
 func (l *PlayerList) Remove(p *Player) {
-
+	for k, v := range l.List {
+		if v.GetID() == p.GetID() {
+			l.List = append(l.List[:k], l.List[k+1:]...)
+		}
+	}
 }
